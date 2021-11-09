e895394ee0f7d10454299d5d7c6d8b0fb8f25c01
Pedro Tashima <pedrotashima42@gmail.com>
2021-11-08 21:44:11 -0300

    feat(handlers): added hello handler

--- main.go
+++ main.go
@@ -1,29 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"github.com/tashima42/go-microservices/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello, world!")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Println(err)
-			http.Error(w, "Bad Request!", http.StatusBadRequest)
-			return
-		}
-		log.Println(string(d))
-		fmt.Fprintf(w, "Hello, %s!", string(d))
-	})
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
 
-	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye, world!")
-		w.Write([]byte("Goodbye, world!"))
-	})
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 
 	http.ListenAndServe(":9090", nil)
 }
